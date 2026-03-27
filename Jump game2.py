3eb1ed3510d0d792bd49af6ac398591ca61af6f8
Madheshwaran V <142651139+Madhesh4@users.noreply.github.com>
2026-03-27 10:26:35 +0530

    Create Jump game2.py

---
+++ Jump game2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return 0
+        
+        jumps = 0
+        current_jump_end = 0
+        farthest = 0
+        
+        for i in range(n - 1):
+            farthest = max(farthest, i + nums[i])
+            
+            if i == current_jump_end:
+                jumps += 1
+                current_jump_end = farthest
+                
+                if current_jump_end >= n - 1:
+                    break
+                    
+        return jumps
