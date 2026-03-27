4fb3523498df33016af7e5444928ebb4b2ecf9af
Madheshwaran V <142651139+Madhesh4@users.noreply.github.com>
2026-03-27 10:33:25 +0530

    Create N-Queens.py

---
+++ N-Queens.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solveNQueens(self, n: int) -> list[list[str]]:
+        results = []
+        cols = set()
+        pos_diag = set()
+        neg_diag = set()
+        board = [["."] * n for _ in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                results.append(["".join(row) for row in board])
+                return
+
+            for c in range(n):
+                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
+                    continue
+
+                cols.add(c)
+                pos_diag.add(r + c)
+                neg_diag.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                cols.remove(c)
+                pos_diag.remove(r + c)
+                neg_diag.remove(r - c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return results
