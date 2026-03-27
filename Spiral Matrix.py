d8c32054d546cf54b65b72e8f88a21e3ca1a5b83
Madheshwaran V <142651139+Madhesh4@users.noreply.github.com>
2026-03-27 10:38:02 +0530

    Create Spiral Matrix.py

---
+++ Spiral Matrix.py
@@ -0,0 +1,30 @@
+class Solution:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        if not matrix:
+            return []
+        
+        result = []
+        rows, cols = len(matrix), len(matrix[0])
+        top, bottom = 0, rows - 1
+        left, right = 0, cols - 1
+        
+        while len(result) < rows * cols:
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1
+            
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+            
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+                
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+                
+        return result
